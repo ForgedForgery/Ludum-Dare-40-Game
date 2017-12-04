7fe7eda29f9d30795d99e5ddbeb1786d9f98bb28
andreas-PC\andreas <hiandreas@googlemail.com>
2017-12-04 13:20:37 +0100

    no message

---
+++ Assets/Scripts/JumpSystem.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpSystem : MonoBehaviour
+{
+    [SerializeField]
+    const int maxJumpAmount = 2;
+    [SerializeField]
+    const float maxJumpCD = 2f;
+
+    private int jumpsUsed = 0;
+    private int jumpsAllowed;
+    private float jumpCD = 0f;
+    private bool onGround = true;
+
+    public delegate void JumpEvents();
+    public static JumpEvents onJump;
+
+    private void Start()
+    {
+        jumpsAllowed = maxJumpAmount;
+        PlayerController.onPlayerPressJump += registerJump;
+        PlayerMotor.onPlayerLand += resetOneJumpOnLanding;
+    }
+
+    private void Update()
+    {
+        if(jumpCD > 0f)
+        {
+            jumpCD -= Time.deltaTime;
+        }
+
+        if(jumpCD <= 0f && jumpsAllowed <= maxJumpAmount)
+        {
+            jumpsAllowed++;
+            jumpCD = maxJumpCD;
+           
+        }
+
+
+        Debug.Log(jumpsAllowed);
+    }
+
+    public void registerJump()
+    {
+        if(jumpsAllowed > 0)
+        {
+            onGround = false;
+            jumpsUsed++;
+            jumpsAllowed--;
+            jumpCD = jumpCD > 0f ? jumpCD : maxJumpCD;
+            onJump();
+        }
+    }
+
+    private void resetOneJumpOnLanding()
+    {
+        if(!onGround)
+        {
+            onGround = true;
+            jumpsAllowed++;
+            
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
+
+    private PlayerMotor motor;
+
+    public delegate void PlayerEvents();
+    public static event PlayerEvents onPlayerPressJump;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        movePlayerWithWASD();
+        moveCamWithMouse();
+        doJumpWithSpace();
+    }
+
+    private void movePlayerWithWASD()
+    {
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.forward * _zMov;
+
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+        motor.setMovement(_velocity);
+    }
+
+    private void doJumpWithSpace()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            if (onPlayerPressJump != null)
+                onPlayerPressJump();
+        }
+    }
+
+    private void moveCamWithMouse()
+    {
+        rotatePlayer();
+        rotateCamAroundPlayer();
+    }
+
+    private void rotatePlayer()
+    {
+        float _yRot = Input.GetAxisRaw("Mouse X");
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        motor.setRotation(_rotation);
+    }
+
+    private void rotateCamAroundPlayer()
+    {
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+        Vector3 _camRotation = new Vector3(-_xRot, 0f, 0f) * lookSensitivity;
+
+        motor.setCamRotation(_camRotation);
+    }
+}
