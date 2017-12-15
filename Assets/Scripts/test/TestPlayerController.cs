06c40b0c212d03462fa1a8ce94c1cb39b45b798d
ForgedForgery <hiandreas@googlemail.com>
2017-12-15 14:41:01 +0100

    Update and rename TestPlayerInput.cs to TestPlayerController.cs

--- Assets/Scripts/test/TestPlayerController.cs
+++ Assets/Scripts/test/TestPlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestPlayerInput : MonoBehaviour
+public class TestPlayerController : MonoBehaviour, ITestInput
 {
     
 }
