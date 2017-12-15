c179415781dfe3396af999c71760fb5fb4d86970
ForgedForgery <hiandreas@googlemail.com>
2017-12-15 14:59:40 +0100

    Update ITestInput.cs

--- Assets/Scripts/test/ITestInput.cs
+++ Assets/Scripts/test/ITestInput.cs
@@ -1,6 +1,6 @@
 public interface ITestInput
 {
     public void readInput();
-    float Rotation { get; }
-    float Velocity { get; }
+    Vector3 Rotation { get; }
+    Vector3 Velocity { get; }
 }
