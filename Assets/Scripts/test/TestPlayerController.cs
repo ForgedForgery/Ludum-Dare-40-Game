1747f75ca3af2b1562e56614ac0a336490f08ae0
ForgedForgery <hiandreas@googlemail.com>
2017-12-15 15:00:27 +0100

    Update TestPlayerController.cs

--- Assets/Scripts/test/TestPlayerController.cs
+++ Assets/Scripts/test/TestPlayerController.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class TestPlayerController : MonoBehaviour, ITestInput
 {
+    private float Rotation { get; }
+    private Vector3 Velocity { get; }
     
+    public void readInput()
+    {
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.forward * _zMov;
+
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+        Velocity = _velocity;
+        
+        float _yRot = Input.GetAxisRaw("Mouse X");
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+        Rotation = _rotation;
+    }
 }
